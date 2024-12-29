637f21075645bd6b51eb29ee42ae20ea8abd2606
Josuesp1620 <josuesp1620@gmail.com>
2024-12-29 17:49:27 +0100

    [Update] Generate random ports and master passwor odoo

--- src/odoo/application/use_cases/create.rs
+++ src/odoo/application/use_cases/create.rs
@@ -14,6 +14,22 @@ impl<R: Repository> CreateUseCase<R> {
     }
 
     pub async fn execute(&self, mut project: ProjectEntity) -> Result<Option<ProjectResponse>, Box<dyn Error>> {
+        if project.random_master_password_odoo && project.master_password_odoo.is_none() {
+            match generate_random_password(24) {
+                Some(password) => {
+                    project.master_password_odoo = Some(password);
+                }
+                None => println!("Error al generar la contraseña."),
+            }
+        }
+        if project.random_port_db && project.port_db.is_none() {
+            let odoo_postgres_port = find_available_port(5000, 5999);
+            project.port_db = Some(odoo_postgres_port);
+        }
+        if project.random_port_web && project.port_web.is_none() {
+            let odoo_web_port = find_available_port(8000, 8999);
+            project.port_web = Some(odoo_web_port);            
+        }
         self.repository.create(project).await
     }    
 }
